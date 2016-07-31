8eca1b82e8e30bb60d3e6f9538ecd5adf633addd
darrennorris <doon75@hotmail.com>
2016-07-31 19:05:27 -0300

    make prettier

--- R/generico.R
+++ R/generico.R
@@ -4,8 +4,8 @@
 #' @author
 #' Created by Victor Lemes Landeiro. Updated 18-07-2008.
 #' @details   
-#' Ported from 
-#' $<https://ppbio.inpa.gov.br/sites/default/files/Generico%20R.doc>$
+#' Ported from:         
+#' https://ppbio.inpa.gov.br/sites/default/files/Generico%20R   
 #' Tried and tested code used to develop package and github.
 #' 
 #' @param tabela A data.frame with the species data. Species in columns.
